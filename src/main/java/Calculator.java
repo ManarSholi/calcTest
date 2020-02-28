50424cdba346522b7515fc0bd4e5ec754c5feaab
AnwarSholi <anwar.ghassan.sholi@gmail.com>
2020-02-28 11:55:22 +0200

    editSrcAndTest

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,26 @@
 package main.java;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Calculator {
-	  public static int evaluate(String expression) {
-	    int sum = 0;
-	    for (String summand: expression.split("\\+"))
-	      sum += Integer.valueOf(summand);
-	    return sum;
-	  }
+	public static int add(int a, int b){
+		int d;
+		d = a+b;
+		return d;
+	}
+	public static void main(String[] args) throws IOException {
+		int c;
+		int b;
+		Scanner in = new Scanner(System.in);
+		b = in.nextInt();
+		int a;
+		a = in.nextInt();
+		if(b >0 && a>0){
+			c = add(b,a);
+			System.out.print(c);
+		}
+		else
+			System.out.println("a<0 or b<0 ");
+	}
 	}
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,15 @@
 package test.java;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import main.java.Calculator;
 
 import org.junit.Test;
 
 public class CalculatorTest {
-  @Test
-  public void evaluatesExpression() {
-    Calculator calculator = new Calculator();
-    int sum = calculator.evaluate("1+2+3");
-    assertEquals(6, sum);
-  }
+	@Test
+	public void testAdd() {
+		Calculator Ob = new Calculator();
+		int act = Ob.add(2,4);
+		assertTrue(6==act);
+	}
 }
