be0a092115a5c87de92355788c8fde3454f357e0
AnwarSholi <anwar.ghassan.sholi@gmail.com>
2020-02-28 11:48:58 +0200

    editting

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,7 @@
 package main.java;
 
 public class Calculator {
-	  public int evaluate(String expression) {
+	  public static int evaluate(String expression) {
 	    int sum = 0;
 	    for (String summand: expression.split("\\+"))
 	      sum += Integer.valueOf(summand);
